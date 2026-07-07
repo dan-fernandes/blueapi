f6369ad0509809b1186da9c55373c604cbb1c42e
dan-fernandes <dan-fernandes@users.noreply.github.com>
2026-07-07 08:27:50 +0000

    deploy: 0ed030ed02fb82d8b050c6a0e0a968f10a3f7381

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.12.1.dev1+g43f608109',
+    VERSION: '1.12.2.dev1+g0ed030ed0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
